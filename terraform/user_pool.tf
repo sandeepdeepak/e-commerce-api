b734e14bb5f845695c4d5e7f5e26d288b8a2c65c
Sandeep Deepak <sandeepdeepak@presidio.com>
2021-03-19 14:08:30 +0530

    pre signup functions added and auto confirming user added

--- terraform/user_pool.tf
+++ terraform/user_pool.tf
@@ -14,3 +14,7 @@ resource "aws_cognito_user_pool_client" "client" {
   user_pool_id        = aws_cognito_user_pool.pool.id
   explicit_auth_flows = ["ADMIN_NO_SRP_AUTH"]
 }
+
+locals {
+  pool_id = aws_cognito_user_pool.pool.id
+}
